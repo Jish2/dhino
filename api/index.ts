8b8a37297a085df332383a6da3b51f0384cc5ddb
Jish2 <j.j.jappiecraft@gmail.com>
2024-06-18 11:25:46 -0700

    format add subpath

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,17 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 
 export const config = {
-  runtime: 'edge'
-}
+  runtime: "edge",
+};
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
+app.get("/", (c) => {
+  return c.json({ message: "Hello Hono!" });
+});
+app.get("/bar", (c) => {
+  return c.json({ message: "Goodbye Hono!" });
+});
 
-export default handle(app)
+export default handle(app);
